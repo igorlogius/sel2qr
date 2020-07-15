1dea65dec18aae65fd1215f58676b4dbf686a163
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-07-15 16:24:41 +0200

    add async + await

--- popup.js
+++ popup.js
@@ -1,46 +1,57 @@
 
 var conPort = browser.runtime.connect({name:"port-from-popup"});
 
-conPort.onMessage.addListener( async  function(m) {
+async function onMessage(m) {
 
-	//console.log("popup.js received message background.js");
+	try {
 
-	let value = '';
-	let noSelection = true;
+		let value = '';
+		let noSelection = true;
 
-	// Order
-	if(typeof m === 'object') {
-		for( b of ['selectionText', 'linkUrl', 'srcUrl', 'pageUrl' ]){
-			if(typeof m[b] === 'string' && m[b].trim() !== ''){
-				value = m[b].trim();
-				//console.log(b,value);
-				noSelection = false;
-				break;
+		// Order
+		if(typeof m === 'object') {
+			for( b of ['selectionText', 'linkUrl', 'srcUrl', 'pageUrl' ]){
+				if(typeof m[b] === 'string' && m[b].trim() !== ''){
+					value = m[b].trim();
+					noSelection = false;
+					break;
+				}
 			}
 		}
+		if(noSelection === true){
+			// Fallback to tab url
+			let tabs = await browser.tabs.query({active: true, currentWindow: true});
+
+			if(!Array.isArray(tabs)){ throw 'tabs is not an array'; }
+			if(tabs.length < 1) { throw 'tabs length is less that 1'; }
+			if(typeof tabs[0].url !== 'string') { throw 'tab url is not a string'; }
+
+			value = tabs[0].url;
+		}
+		
+		// clear previous selection
+		await conPort.postMessage("clear");
+
+
+		new QRious({
+			background: "white",
+			backgroundAlpha: 1.0,
+			element: document.getElementById('qr'),
+			foreground: "black",
+			foregroundAlpha: 1.0,
+			level: "L",
+			mime: "image/png",
+			size: window.innerWidth,
+			value: value
+		});
+
+		//console.log('qr value', value);
+
+	}catch(e){
+		console.error(e);
 	}
-	if(noSelection === true){
-		// Fallback to tab url
-		let tabs = await browser.tabs.query({active: true, currentWindow: true});
-		value = tabs[0].url;
-	}
 
-	//console.log('qr value', value);
-	
-	var qr = new QRious({
-		background: "white",
-		backgroundAlpha: 1.0,
-		element: document.getElementById('qr'),
-		foreground: "black",
-		foregroundAlpha: 1.0,
-		level: "L",
-		mime: "image/png",
-		size: window.innerWidth,
-		value: value
-	});
+}
 
-	// clear previous selection
-	conPort.postMessage("clear");
-
-});
+conPort.onMessage.addListener(onMessage);
 
